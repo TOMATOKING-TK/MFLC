6f60afa99c8dcbe97ed2d752d9470c840f3273aa
TOMATOKING-TK <rluca7725@gmail.com>
2023-10-19 16:43:19 +0200

    added the code lol

---
+++ main.py
@@ -0,0 +1,76 @@
+import os
+import math
+
+add_per_level = 160
+first_level = 200
+kill_XP = 50
+
+options = """
+[1] how much more xp is needed for your next level goal
+[2] how much xp is needed to go to that level from 0
+"""
+
+chose_list = [
+    1,
+    2
+]
+
+def calculator_from(start_level, go_level):
+    os.system('cls')
+    XP_amount = 0
+    start_level_XP = 0
+    base_level_XP = add_per_level * start_level; base_level_XP += first_level
+    go_level_XP = add_per_level * go_level; go_level_XP += first_level
+    needed_XP = base_level_XP - go_level_XP
+    level_needed = go_level - start_level
+    for I in range(start_level):
+        start_level_XP_cal = add_per_level * I; start_level_XP_cal += first_level; start_level_XP += start_level_XP_cal
+    for I in range(go_level):
+        go_level_XP = add_per_level * I; go_level_XP += first_level; XP_amount += go_level_XP
+    XP_amount = XP_amount - start_level_XP
+    kills_needed = XP_amount / kill_XP; kills_needed = math.ceil(kills_needed)
+    print("these are the stats for your wanted level:")
+    print("levels needed: " + str(level_needed))
+    print("XP needed: " + str(XP_amount))
+    print("total kills needed: " + str(int(kills_needed)))
+    input("press enter to go back")
+
+def calculator_to(start_level):
+    os.system('cls')
+    XP_amount = 0
+    for I in range(start_level):
+        go_level_XP = add_per_level * I; go_level_XP += first_level; XP_amount += go_level_XP
+    print("level: " + str(start_level))
+    print("XP needed: " + str(XP_amount))
+    input("press enter to go back")
+
+def main():
+    os.system('cls')
+    print(options)
+    chose = input()
+    
+    try:
+        chose = int(chose)
+    except:
+        return
+    
+    if chose in chose_list:
+        if chose == 1:
+            start_level = input("what is your starting level: ")
+            go_level = input("what is your level goal: ")
+            try:
+                start_level = int(start_level)
+                go_level = int(go_level)
+            except:
+                return
+            calculator_from(start_level, go_level)
+        else:
+            start_level = input("level: ")
+            try:
+                start_level = int(start_level)
+            except:
+                return
+            calculator_to(start_level)
+
+while True:
+    main()
