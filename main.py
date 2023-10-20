8dcc86ae8c16fd12248619960b231cd822e6c8cc
TOMATOKING-TK <rluca7725@gmail.com>
2023-10-20 10:36:23 +0200

    code optimization
    i made the script at college and was multitasking while doing so
    
    so mistakes were made while coding this commit aims to remove/fix a lot of the mistakes that i made
    
    i will continue to make improvements or even add new features to the script

--- main.py
+++ main.py
@@ -24,20 +24,10 @@ def get_start_level_XP(start_level):
         start_level_XP_cal = add_per_level * I; start_level_XP_cal += first_level; start_level_XP += start_level_XP_cal
     return start_level_XP
 
-def get_go_level_XP(go_level):
-    XP_amount = 0
-    for I in range(go_level):
-        go_level_XP = add_per_level * I; go_level_XP += first_level; XP_amount += go_level_XP
-    return XP_amount
-
 def calculator_from(start_level, go_level):
     clear()
-    base_level_XP = add_per_level * start_level; base_level_XP += first_level
-    go_level_XP = add_per_level * go_level; go_level_XP += first_level
-    needed_XP = base_level_XP - go_level_XP
     level_needed = go_level - start_level
-    start_level_XP = get_start_level_XP(start_level)
-    XP_amount = get_go_level_XP(go_level)
+    start_level_XP = get_start_level_XP(start_level); XP_amount = get_start_level_XP(go_level)
     XP_amount = XP_amount - start_level_XP
     kills_needed = XP_amount / kill_XP; kills_needed = math.ceil(kills_needed)
     print("these are the stats for your wanted level:")
@@ -45,18 +35,19 @@ def calculator_from(start_level, go_level):
     print("wanted level: " + str(go_level))
     print("levels needed: " + str(level_needed))
     print("XP needed: " + str(XP_amount))
-    print("total kills needed: " + str(int(kills_needed)))
+    print("raw kills needed: " + str(int(kills_needed)))
     input("press enter to go back")
 
 def calculator_to(start_level):
     clear()
-    XP_amount = get_go_level_XP(start_level)
+    XP_amount = get_start_level_XP(start_level)
+    level_UP = get_start_level_XP(start_level + 1); level_UP = level_UP - XP_amount
     print("level: " + str(start_level))
+    print(f"XP needed for {start_level} to {start_level + 1}: {level_UP}")
     print("XP needed: " + str(XP_amount))
     input("press enter to go back")
 
 def main():
-    clear()
     print(options)
     chose = input()
     try:
@@ -73,13 +64,17 @@ def main():
             except:
                 return
             calculator_from(start_level, go_level)
-        else:
+        elif chose == 2:
             start_level = input("level: ")
             try:
                 start_level = int(start_level)
             except:
                 return
             calculator_to(start_level)
+    else:
+        print(f"\n{chose} is not an option")
+        input("press enter to go back")
 
 while True:
+    clear()
     main()
